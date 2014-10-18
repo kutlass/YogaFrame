6f322028010e4225e73f9a204ec370c4e65590a2
kutlass <kutlass@yogaframe.net>
2014-10-18 12:08:31 -0700

    Add MySQL databinding prototype UnitySaturday. Think i forgot to save the Scene file so i'll have to rebuild it on a subsequent checkin

---
+++ prototype/UnitySaturday/Assets/ConfusedScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfusedScript : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		WWW www = new WWW ("https://www.yogaframe.net/hello.php");
+		yield return www;
+		Debug.Log ("ConfusedScript.Start(): Spitting something from yogaframe.net: " + www.text);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
---
+++ prototype/UnitySaturday/Assets/TextHelper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextHelper : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+	
+		WWW www = new WWW ("https://www.yogaframe.net/hello.php");
+		yield return www;
+		Debug.Log ("TextHelper.Start(): Spitting something from yogaframe.net: " + www.text);
+		Text text = GetComponent<Text> ();
+		text.text = www.text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
