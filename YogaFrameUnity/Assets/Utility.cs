dae4d919ff11e473b3a10412bd1a414535c6e2b1
kutlass <kutlass@yogaframe.net>
2014-10-20 17:23:16 -0700

    Add Unity.cs for PHPConnect databind proof. Permit *.sln in .gitignore to enforce spaced-tabbing

---
+++ YogaFrameUnity/Assets/Utility.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Utility : MonoBehaviour
+{
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        WWW www = new WWW ("https://www.yogaframe.net/Connect.php");
+        yield return www;
+        Debug.Log ("Utility.Start(): Spitting something from yogaframe.net: " + www.text);
+        Text text = GetComponent<Text> ();
+        text.text = www.text;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
