2779ed0ab0175f3564bb97852c019d0ad926fda4
kutlass <kutlass@yogaframe.net>
2014-10-30 09:43:57 -0700

    Stub class TestData.cs. Proofs for Dappler client data flow and sorting.

---
+++ prototype/YogaFrameWebAdapter/TestData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YogaFrameWebAdapter
+{
+    //
+    // This TestData will act as stub data in lieu of actual database access.
+    // Focus is to get Dappler data flow functioning as expected for the Client app
+    //
+    class TestData
+    {
+        List<Dapple> m_listDapples;
+
+        //
+        // Private methods
+        //
+        private void _GenerateTestData()
+        {
+            m_listDapples.Add(new Dapple());
+        }
+
+        //
+        // Constructor
+        //
+        public TestData()
+        {
+            m_listDapples = new List<Dapple>();
+        }
+    }
+}
