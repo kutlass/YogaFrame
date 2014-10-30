45e9b402ea75aef7b0751d6deb235fefc0c282ac
kutlass <kutlass@yogaframe.net>
2014-10-30 13:27:16 -0700

    Psuedo code for WebGet initialization and corresponding stub classes

--- prototype/YogaFrameWebAdapter/WebAdapter.cs
+++ prototype/YogaFrameWebAdapter/WebAdapter.cs
@@ -15,5 +15,37 @@ namespace YogaFrameWebAdapter
             // TODO: Return List<Dapple>() from TestData.cs
             return new List<Dapple>();
         }
+
+        //
+        // A series of pseudo methods for now:
+        //
+        public void Initialize()
+        {
+            WebGetGames();
+            WebGetCharacters();
+            WebGetMoves();
+            WebGetInputSchema();
+            WebGetInputSequence();
+        }
+        public Game WebGetGames()
+        {
+            return new Game();
+        }
+        public Character WebGetCharacters()
+        {
+            return new Character();
+        }
+        public Move WebGetMoves()
+        {
+            return new Move();
+        }
+        public InputSchema WebGetInputSchema()
+        {
+            return new InputSchema();
+        }
+        public InputSequence WebGetInputSequence()
+        {
+            return new InputSequence();
+        }
     }
 }
