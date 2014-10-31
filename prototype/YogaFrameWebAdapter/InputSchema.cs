6e449d05919e9186b2646241502f7b44c1e47b6b
kutlass <kutlass@yogaframe.net>
2014-10-30 19:43:49 -0700

    enum UniversalButtons

--- prototype/YogaFrameWebAdapter/InputSchema.cs
+++ prototype/YogaFrameWebAdapter/InputSchema.cs
@@ -5,6 +5,28 @@ using System.Text;
 
 namespace YogaFrameWebAdapter
 {
+    //
+    // UniversalButtons will be one thing unchangeable by #FGC
+    // Some things must be absolute in order to be built upon
+    //
+    enum UniversalButtons
+    {
+        // Visualize list via 360 motion starting Down and ending DownBack:
+        Down,
+        DownForward,
+        Forward,
+        UpForward,
+        Up,
+        UpBack,
+        Back,
+        DownBack,
+        // End 360 motion
+        
+        // 6 button stick layout:
+        A, B, C,
+        X, Y, Z
+    };
+
     class InputSchema
     {
     }
