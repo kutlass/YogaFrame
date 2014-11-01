227ea0ebe5b44e07bce8610cc75ae74c0d721384
kutlass <kutlass@yogaframe.net>
2014-11-01 07:47:40 -0700

    Fields for Move.cs

--- prototype/YogaFrameWebAdapter/Move.cs
+++ prototype/YogaFrameWebAdapter/Move.cs
@@ -8,5 +8,34 @@ namespace YogaFrameWebAdapter
     public class Move
     {
         public Dappler m_dappler;
+        public List<MoveColumn> m_listMoveColumns;
+
+        //
+        // Constructor
+        //
+        public Move(Dappler dappler, List<MoveColumn> listMoveColumns)
+        {
+            m_dappler = dappler;
+            m_listMoveColumns = listMoveColumns;
+        }
     }
+
+    public class MoveColumn
+    {        
+        public Dappler m_dappler;
+        
+        public MoveColumnType m_movementColumnType;
+        
+        public string m_name;
+        public int m_valueInteger;
+        public string m_valueText;
+        public string m_valueBolean;
+    }
+
+    public enum MoveColumnType
+    {
+        integer,
+        text,
+        boolean
+    };
 }
