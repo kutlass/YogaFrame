ceb5d0a197fa564ff5a32bade39cb1f1b0987b11
kutlass <kutlass@yogaframe.net>
2014-11-07 20:41:30 -0800

    Checkin MySQL Connector tutorial code into YogaFrameDeploy and ensure build succeeds.

--- prototype/YogaFrameDeploy/Deployment.cs
+++ prototype/YogaFrameDeploy/Deployment.cs
@@ -10,7 +10,54 @@ namespace YogaFrameDeploy
     {
         public void DatabaseConnect()
         {
-            MySqlConnection connection = new MySqlConnection();
+            //
+            // PLACEHOLDER TUTORIAL CODE - Making a MySQL connection
+            //
+            string connStr = "server=localhost;user=root;database=world;port=3306;password=******;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                // Perform database operations
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+
+        public void DatabaseCommand()
+        {
+            //
+            // PLACEHOLDER TUTORIAL CODE - Constructing a MySQL command
+            //
+            string connStr = "server=localhost;user=root;database=world;port=3306;password=******;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "SELECT Name, HeadOfState FROM Country WHERE Continent='Oceania'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
         }
         
         public static void DatabaseDeploy()
