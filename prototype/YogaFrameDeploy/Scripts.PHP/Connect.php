c088acaf7b1a0570db0b112364bb049eb7ff92ac
kutlass <kutlass@yogaframe.net>
2014-11-12 19:23:21 -0800

    New Connect.php script with YogaConnect() db connect function.

---
+++ prototype/YogaFrameDeploy/Scripts.PHP/Connect.php
@@ -0,0 +1,32 @@
+﻿<html>
+ <head>
+  <title>PHP YogaFrame Connection Test</title>
+ </head>
+ <body>
+
+<?php
+
+require_once ('../../ConnectData.php');
+
+function YogaConnect()
+{
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if (true != $mysqli->connect_errno)
+    {
+        echo "mysqli() connection succeeded.";
+        echo nl2br("\n\r");
+        echo $mysqli->host_info . "\n\r";
+        echo nl2br("\n\r");
+    }
+    else
+    {
+        echo "mysqli() connection failed: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+    	
+    return $mysqli;
+}
+
+?>
+ 
+ </body>
+</html>
