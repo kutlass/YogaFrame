5f6984f2d90bfdc728bae1d13b3e8cb6a1906ee5
Karl Flores <kutlass@yogaframe.net>
2014-11-29 13:59:55 -0800

    Make unimplemented test cases fail by default. Remove sample code.

--- prototype/UnitTests/UnitTests.cs
+++ prototype/UnitTests/UnitTests.cs
@@ -18,26 +18,29 @@ namespace UnitTests
         [Test]
         public void DatabaseConnect()
         {
-            Deployment deployment = new Deployment();
-            //deployment.DatabaseConnect();
+            // TODO: Write unit test. Fail test case while not implemented
+            Assert.AreEqual(1, 2);
         }
 
         [Test]
         public void DatabaseDeploy()
         {
-            Deployment.DatabaseBackup();
+            // TODO: Write unit test. Fail test case while not implemented
+            Assert.AreEqual(1, 2);
         }
 
         [Test]
         public void DatabaseBackup()
         {
-            Deployment.DatabaseBackup();
+            // TODO: Write unit test. Fail test case while not implemented
+            Assert.AreEqual(1, 2);
         }
 
         [Test]
         public void DatabaseRestore()
         {
-            //Deployment.tbl_Characters_insert();
+            // TODO: Write unit test. Fail test case while not implemented
+            Assert.AreEqual(1, 2);
         }
     }
 
@@ -46,31 +49,9 @@ namespace UnitTests
     {
         [Test]
         public void GetGames()
-        {     
-            WebAdapter.WebGetGames();
-
-            List<Game> listGames = TestData.GenerateGames();
-
-            Game game = listGames.ElementAt(0);
-            
-            Assert.AreNotEqual("STRENGTH", game.m_listGameAttributes.ElementAt(0).m_name);
-            Assert.AreEqual(TestData.GAME_NAME_PAIR_KEY, game.m_listGameAttributes.ElementAt(0).m_name);
-            Assert.AreEqual(TestData.GAME_NAME_PAIR_VALUE, game.m_listGameAttributes.ElementAt(0).m_value);
-
-            Assert.AreEqual(TestData.GAME_DEVELOPER_PAIR_KEY, game.m_listGameAttributes.ElementAt(1).m_name);
-            Assert.AreEqual(TestData.GAME_DEVELOPER_PAIR_VALUE, game.m_listGameAttributes.ElementAt(1).m_value);
-
-            Assert.AreEqual(TestData.GAME_DEVELOPER_URL_PAIR_KEY, game.m_listGameAttributes.ElementAt(2).m_name);
-            Assert.AreEqual(TestData.GAME_DEVELOPER_URL_PAIR_VALUE, game.m_listGameAttributes.ElementAt(2).m_value);
-
-            Assert.AreEqual(TestData.GAME_PUBLISHER_PAIR_KEY, game.m_listGameAttributes.ElementAt(3).m_name);
-            Assert.AreEqual(TestData.GAME_PUBLISHER_PAIR_VALUE, game.m_listGameAttributes.ElementAt(3).m_value);            
-
-            Assert.AreEqual(TestData.GAME_PUBLISHER_URL_PAIR_KEY, game.m_listGameAttributes.ElementAt(4).m_name);
-            Assert.AreEqual(TestData.GAME_PUBLISHER_URL_PAIR_VALUE, game.m_listGameAttributes.ElementAt(4).m_value);
-
-            Assert.AreEqual(TestData.GAME_DESCRIPTION_PAIR_KEY, game.m_listGameAttributes.ElementAt(5).m_name);
-            Assert.AreEqual(TestData.GAME_DESCRIPTION_PAIR_VALUE, game.m_listGameAttributes.ElementAt(5).m_value);
+        {
+            // TODO: Write unit test. Fail test case while not implemented
+            Assert.AreEqual(1, 2);
         }
 
         [Test]
@@ -218,40 +199,27 @@ namespace UnitTests
         public void GetMoves()
         {
             WebAdapter.WebGetMoves();
+
+            // TODO: Write unit test. Fail test case while not implemented
+            Assert.AreEqual(1, 2);
         }
 
         [Test]
         public void GetInputSequence()
         {
             WebAdapter.WebGetInputSequence();
+
+            // TODO: Write unit test. Fail test case while not implemented
+            Assert.AreEqual(1, 2);
         }
 
         [Test]
         public void GetInputSchema()
         {
             WebAdapter.WebGetInputSchema();
-        }
-    }
 
-    //
-    // TODO: Remove AccountTest. This is cut and paste gunk from NUnit tutorial
-    //
-    [TestFixture]
-    public class AccountTest
-    {
-        [Test]
-        public void TransferFunds()
-        {
-            Account source = new Account();
-            source.Deposit(200m);
-
-            Account destination = new Account();
-            destination.Deposit(150m);
-
-            source.TransferFunds(destination, 100m);
-
-            Assert.AreEqual(250m, destination.Balance);
-            Assert.AreEqual(100m, source.Balance);
+            // TODO: Write unit test. Fail test case while not implemented
+            Assert.AreEqual(1, 2);
         }
     }
 }
