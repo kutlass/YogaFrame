26cced4c1717b05fa52413605226bf70f8d643e6
Karl Flores <kutlass@yogaframe.net>
2014-12-25 02:05:20 -0800

    PHP scripts for WebGetSessions and WebPostSession APIs.

---
+++ prototype/YogaFrameDeploy/Scripts.PHP/home3.yogafram/public_html/YogaFrame/GetSessions.php
@@ -0,0 +1,58 @@
+﻿<?php
+
+require_once ('./Util.php');
+
+//
+// - Create one master array for the records
+// - Pass array by reference to be filled by the GetSessions() stored proc helper
+// - Pass newly-filled resultset array to the JSON encoder
+//
+$tbl_Sessions = array();
+$strStoredProcedureName = "GetSessions()";
+$fResult = false;
+$fResult = GetSessionsHelper::FetchDataViaStoredProcedure($strStoredProcedureName, $tbl_Sessions);
+if (true == $fResult)
+{
+    $fResult = GetSessionsHelper::EncodeJson($tbl_Sessions);
+}
+
+class GetSessionsHelper
+{
+    public static function FetchDataViaStoredProcedure($strStoredProcedureName, /*ref*/ &$tbl_Sessions)
+    {
+        $fResult = false;
+        $fResult = Util::ExecuteQueryReadOnly($strStoredProcedureName, $tbl_Sessions);
+        
+        return $fResult;
+    }
+    
+    public static function EncodeJson($tbl_Sessions)
+    {
+        $fResult = false;
+
+        //
+        // Format the master array into JSON encoding
+        //
+        Trace::WriteLine("GetSessionsHelper::EncodeJson: ");
+        $json_encode = json_encode( array('tbl_InputSequences'=>$tbl_Sessions));
+        if (FALSE != $json_encode)
+        {
+            Trace::WriteLine("GetSessionsHelper::EncodeJson: echoing json_encode() value...");
+            Trace::EchoJson($json_encode);
+            
+            //
+            // If we got this far, the entire function succeeded:
+            //
+            $fResult = true;
+        }
+        else
+        {
+            $fResult = false;
+            Trace::WriteLineFailure("GetSessionsHelper::EncodeJson: json_encode() returned FALSE.");
+        }
+        
+        return $fResult;
+    }
+}
+
+?>
---
+++ prototype/YogaFrameDeploy/Scripts.PHP/home3.yogafram/public_html/YogaFrame/PostSession.php
@@ -0,0 +1,77 @@
+﻿<?php
+
+require_once ('./Util.php');
+
+//
+// - Deserialize the json-encoded http POST payload string
+// - Convert deserialized PHP object into custom Sessions object
+// - Call stored procedure, passing Sessions instance fields as input
+//
+$deserializedPhpObjectFromJson = json_decode($_POST['json']);
+if (null != $deserializedPhpObjectFromJson)
+{
+    var_dump($deserializedPhpObjectFromJson);
+    $sessions = Sessions::CreateInstanceFromJson($deserializedPhpObjectFromJson);
+    
+    $valGuidSession      = $sessions->TblSession[0]->guidSession;
+    $valIdtblMembers     = $sessions->TblSession[0]->idtblMembers;
+    $valDtLastHeartBeat  = $sessions->TblSession[0]->dtLastHeartBeat;
+
+    $strQuery =
+        "CALL PostSession(" .
+        "'"                 . $valGuidSession      . "'," .
+        "'"                 . $valIdtblMembers     . "'," .
+        "'"                 . $valDtLastHeartBeat  . "'"  .
+        ")";
+    
+    $mysqli = Util::YogaConnect();
+    if (null != $mysqli)
+    {
+        $fResult = false;
+        $fResult = Util::ExecuteQuery($mysqli, $strQuery);
+        if (true != $fResult)
+        {
+            Trace::WriteLineFailure("PostSession.php: Failed to call the stored procedure.");
+        }
+        
+        $mysqli->close();
+    }
+}
+else
+{
+    Trace::WriteLineFailure("PostSession.php: Failure: null object returned from json_decode.");
+}
+
+//
+// Object representation of client-submitted payload
+//
+class TblSession
+{
+    public $ColName;
+    public $IdtblDapplers;
+}
+
+class Sessions
+{
+    public $TblSession;
+    
+    public static function CreateInstanceFromJson($deserializedPhpObjectFromJson)
+    {
+        //
+        // Manually reconstruct my user-defined PHP object: Sessions
+        //
+        $arraySource = $deserializedPhpObjectFromJson->tbl_Sessions;
+        $sessions = new Sessions();
+        $sessions->TblSession = array( new TblSession() );
+        for ($i = 0; $i < count($arraySource); $i++)
+        {
+            $sessions->TblSession[$i]->GuidSession     = $arraySource[$i]->guidSession;
+            $sessions->TblSession[$i]->IdtblMembers    = $arraySource[$i]->idtblMembers;
+            $sessions->TblSession[$i]->DtLastHeartBeat = $arraySource[$i]->dtLastHeartBeat;
+        }
+        
+        return $sessions;
+    }
+}
+
+?>
