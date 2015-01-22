26ed42cf081fb698ff89e79868d490719479f969
Karl Flores <kutlass@yogaframe.net>
2015-01-22 10:10:07 -0800

    Unity3D textbox displays string pulled from YogaFrameWebAdapter.dll

--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/MyUnityScript.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/MyUnityScript.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class MyUnityScript : MonoBehaviour {
+public class MyUnityScript : MonoBehaviour
+{
 	public YogaFrameUnityAdapter yogaFrameUnityAdapter;
-
-	// Use this for initialization
-	void Start () {
-		yogaFrameUnityAdapter = new YogaFrameUnityAdapter ();
-		print (yogaFrameUnityAdapter.GetInstance ());
-	}
 	
+	// Use this for initialization
+	void Start ()
+	{
+		yogaFrameUnityAdapter = new YogaFrameUnityAdapter ();
+		Text text = GetComponent<Text>();
+		text.text = yogaFrameUnityAdapter.GetInstance();
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
