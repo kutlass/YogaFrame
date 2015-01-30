9524a76110af34aad562aec793a4f887eb965998
Karl Flores <kutlass@yogaframe.net>
2015-01-30 11:49:38 -0800

    UI now pushes real Username and Password inputs to webservice via [Sign in] button.

--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/MyUnityScript.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/MyUnityScript.cs
@@ -7,34 +7,46 @@ using YogaFrameWebAdapter.Session;
 
 public class MyUnityScript : MonoBehaviour
 {
-	public Text textStatusSignIn;
-	public InputField inputFieldUserName;
-	public InputField inputFieldPassword;
-
-	// Use this for initialization
-	void Start()
+	public Text m_textStatusSignIn;
+	public InputField m_inputFieldUserName;
+	public InputField m_inputFieldPassword;
+	
+	public void SignInUI()
 	{
+		Text[] texts;
+		texts = GetComponentsInChildren<Text>();
+		foreach (Text text in texts) 
+		{
+			if ("textStatusSignIn" == text.name)
+			{
+				m_textStatusSignIn = text;
+			}
+		}
+		InputField[] inputFields;
+		inputFields = GetComponentsInChildren<InputField>();
+		foreach (InputField inputField in inputFields)
+		{
+			if ("inputFieldUserName" == inputField.name)
+			{
+				m_inputFieldUserName = inputField;
+			}
+			if ("inputFieldPassword" == inputField.name)
+			{
+				m_inputFieldPassword = inputField;
+			}
+		}
 
+		SignIn(m_inputFieldUserName.text, m_inputFieldPassword.text);
 	}
 
-	// Update is called once per frame
-	void Update()
+	public void SignIn(string strUserName, string strPassword)
 	{
-
-	}
-
-	public void SignIn(string text)
-	{
-		textStatusSignIn = GetComponent<Text>();
-		inputFieldUserName = GetComponent<InputField>();
-		inputFieldPassword = GetComponent<InputField>();
-
 		JSession jSessionWebResponse = null;
-		jSessionWebResponse = Session.MemberSignIn(inputFieldUserName.text, inputFieldPassword.text);
+		jSessionWebResponse = Session.MemberSignIn(strUserName, strPassword);
 		//jSessionWebResponse = Session.MemberSignIn("kutlass", "PoweredBy#FGC8675309");
 		if (null != jSessionWebResponse)
 		{
-			textStatusSignIn.text = jSessionWebResponse.Dispatch.Message;
+			m_textStatusSignIn.text = jSessionWebResponse.Dispatch.Message;
 		}
 	}
 }
