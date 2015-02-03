181960e4bfea291f1b3d68c2e79b278fa672a70b
kutlass <kutlass@yogaframe.net>
2015-02-03 13:27:50 -0800

    Implement column toggling between SignIn and SignUp scenes.

---
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptSceneManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScriptSceneManager : MonoBehaviour
+{
+	public void SwitchToScene(string strSceneToSwitchTo)
+	{
+		Application.LoadLevel(strSceneToSwitchTo);
+	}
+}
