9d0c9cd1ce1fe26202d50bba70858f1780a4835e
kutlass <kutlass@yogaframe.net>
2015-02-11 07:38:36 -0800

    Make ClickableText.prefab support dynamic text and work within an AutoLayout panel.

---
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ClickableText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClickableText : MonoBehaviour
+{
+	public Text m_textClickableText;
+
+	// Use this for initialization
+	void Start()
+	{
+		m_textClickableText.text = "Dynamically generated text.";
+	}
+}
--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptClickableText.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptClickableText.cs
@@ -5,12 +5,24 @@ using System.Collections;
 public class ScriptClickableText : MonoBehaviour
 {
 	public GameObject m_panelColumnFrameData;
+
 	public GameObject m_prefabClickableText;
+	public GameObject m_prefabClickableText2;
+	public GameObject m_prefabClickableText3;
 
 	// Use this for initialization
 	void Start()
 	{
-		m_prefabClickableText = Instantiate(Resources.Load("Prefabs/ClickableText"), new Vector3 (200, 200, 0), Quaternion.identity) as GameObject;
+		//m_prefabClickableText = Instantiate(Resources.Load("Prefabs/ClickableText"), new Vector3 (200, 200, 0), Quaternion.identity) as GameObject;
+		//m_prefabClickableText2 = Instantiate(Resources.Load("Prefabs/ClickableText"), new Vector3 (200, 300, 0), Quaternion.identity) as GameObject;
+		//m_prefabClickableText3 = Instantiate(Resources.Load("Prefabs/ClickableText"), new Vector3 (200, 400, 0), Quaternion.identity) as GameObject;
+
+		m_prefabClickableText = Instantiate(Resources.Load("Prefabs/ClickableText")) as GameObject;
+		m_prefabClickableText2 = Instantiate(Resources.Load("Prefabs/ClickableText")) as GameObject;
+		m_prefabClickableText3 = Instantiate(Resources.Load("Prefabs/ClickableText")) as GameObject;
+
 		m_prefabClickableText.transform.SetParent(m_panelColumnFrameData.transform);
+		m_prefabClickableText2.transform.SetParent(m_panelColumnFrameData.transform);
+		m_prefabClickableText3.transform.SetParent(m_panelColumnFrameData.transform);
 	}
 }
