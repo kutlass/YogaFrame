7c433485c9395740bbf8d63fa004ecb8cb501ffa
Karl Flores <kutlass@yogaframe.net>
2015-03-16 10:30:18 -0700

    Bind game Description and Publisher to GameInfo scene with real cursor positions.

--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptGameInfo.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptGameInfo.cs
@@ -6,12 +6,16 @@ using YogaFrameWebAdapter.Session;
 public class ScriptGameInfo : MonoBehaviour
 {
 	public Text m_textGameName;
+	public Text m_textGamePublisher;
+	public Text m_textGameDescription;
 
 	// Use this for initialization
 	void Start ()
 	{
 		int position = Session.Instance.Cache.GamesPositionLastSelected;
 		m_textGameName.text = Session.Instance.Cache.Games.TblGames[position].ColName;
+		m_textGamePublisher.text = Session.Instance.Cache.Games.TblGames[position].ColPublisher;
+		m_textGameDescription.text = Session.Instance.Cache.Games.TblGames[position].ColDescription;
 	}
 	
 	// Update is called once per frame
