a3598ffa4f758771fcaf32fd2122500ba4490aea
Karl Flores <kutlass@yogaframe.net>
2015-03-16 09:56:04 -0700

    Bind GameInfo scene to real cursor postions from Games cache.

--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ClickableText.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ClickableText.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using YogaFrameWebAdapter.Session;
 
 public class ClickableText : MonoBehaviour
 {
@@ -11,5 +12,15 @@ public class ClickableText : MonoBehaviour
 	void Start()
 	{
 		//m_textClickableText.text = "Dynamically generated text.";
+	}
+
+	public void SetEntryPointPosition(int entryPointPosition)
+	{
+		Session.Instance.Cache.GamesPositionLastSelected = m_entryPointPosition;
+	}
+
+	public int GetEntryPointPosition(int entryPointPosition)
+	{
+		return m_entryPointPosition;
 	}
 }
---
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptGameInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using YogaFrameWebAdapter.Session;
+
+public class ScriptGameInfo : MonoBehaviour
+{
+	public Text m_textGameName;
+
+	// Use this for initialization
+	void Start ()
+	{
+		int position = Session.Instance.Cache.GamesPositionLastSelected;
+		m_textGameName.text = Session.Instance.Cache.Games.TblGames[position].ColName;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}	
+}
