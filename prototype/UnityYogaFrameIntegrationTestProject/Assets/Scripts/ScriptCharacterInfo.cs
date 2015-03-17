749c4a58e74ba2bd2e6cd404f33b2449bc71ad6f
kutlass <kutlass@yogaframe.net>
2015-03-17 10:01:11 -0700

    Stub scene for CharacterInfo and corresponding script.

---
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptCharacterInfo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using YogaFrameWebAdapter;
+using YogaFrameWebAdapter.Session;
+using YogaFrameWebAdapter.CharactersJsonTypes;
+using YogaFrameWebAdapter.MovesJsonTypes;
+using System.Collections;
+
+public class ScriptCharacterInfo : MonoBehaviour
+{
+	public Text m_characterName;
+	public Text m_characterDescription;
+	public GameObject m_panelMovesList;
+	public GameObject[] m_rgPrefabClickableTexts;
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
