9d69994b9401c0a43b2bb297869bc02f244e8592
kutlass <kutlass@yogaframe.net>
2015-03-18 11:14:54 -0700

    Only show game-specific characters within the CharacterInfo scene.

--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptGameInfo.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptGameInfo.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 using YogaFrameWebAdapter;
 using YogaFrameWebAdapter.Session;
 using YogaFrameWebAdapter.JSessionJsonTypes;
@@ -40,15 +41,29 @@ public class ScriptGameInfo : MonoBehaviour
 		//
 		// Instantiate a ClickableText.prefab for each Character element cached from the web service
 		//
-		m_rgPrefabClickableTexts = new GameObject[m_characters.TblCharacters.Length];
-		for (int i = 0; i < m_characters.TblCharacters.Length; i++)
+		// Create a view of only Characters relevant to the Game at hand
+		int GAME_POSITION_LAST_SELECTED = Session.Instance.Cache.GamesPositionLastSelected;
+		string GAME_ID = Session.Instance.Cache.Games.TblGames[GAME_POSITION_LAST_SELECTED].IdtblGames;
+		List<TblCharacter> listTblCharacters = new List<TblCharacter>();
+		for (int x = 0; x < m_characters.TblCharacters.Length; x++)
+		{
+			TblCharacter currentCharacter = m_characters.TblCharacters[x];
+			if (GAME_ID == currentCharacter.IdtblGames)
+			{
+				listTblCharacters.Add(currentCharacter);
+			}
+		}
+		// Allocate array count based on Game-specific Characters found
+		m_rgPrefabClickableTexts = new GameObject[listTblCharacters.Count];
+		TblCharacter[] rgCharacters = listTblCharacters.ToArray();
+		for (int i = 0; i < m_rgPrefabClickableTexts.Length; i++)
 		{
 			m_rgPrefabClickableTexts[i] = Instantiate(Resources.Load("Prefabs/ClickableText")) as GameObject;
 			m_rgPrefabClickableTexts[i].transform.SetParent(m_panelCharactersList.transform);
-			Text textClickableText = m_rgPrefabClickableTexts[i].GetComponentInChildren<Text>();
 			ClickableText clickableText = m_rgPrefabClickableTexts[i].GetComponentInChildren<ClickableText>();
 			clickableText.m_entryPointPosition = i;
-			textClickableText.text = m_characters.TblCharacters[i].ColName;
+			Text textClickableText = m_rgPrefabClickableTexts[i].GetComponentInChildren<Text>();
+			textClickableText.text = rgCharacters[i].ColName;
 		}
 	}
 }
