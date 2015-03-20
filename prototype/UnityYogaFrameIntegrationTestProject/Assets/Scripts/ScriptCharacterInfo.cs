53ba161c8927803ebeaf186fb81add9f701faf3e
Karl Flores <kutlass@yogaframe.net>
2015-03-20 09:09:50 -0700

    UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptCharacterInfo.cs

--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptCharacterInfo.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptCharacterInfo.cs
@@ -24,4 +24,38 @@ public class ScriptCharacterInfo : MonoBehaviour
 	{
 	
 	}
+
+	// TODO: Refactor _PopulateCharactersList() into _PopulateMovesList()
+	private void _PopulateCharactersList()
+	{
+		m_characters = Session.Instance.Cache.Characters;
+		
+		//
+		// Instantiate a ClickableText.prefab for each Character element cached from the web service
+		//
+		// Create a view of only Characters relevant to the Game at hand
+		int GAME_POSITION_LAST_SELECTED = Session.Instance.Cache.GamesPositionLastSelected;
+		string GAME_ID = Session.Instance.Cache.Games.TblGames[GAME_POSITION_LAST_SELECTED].IdtblGames;
+		List<TblCharacter> listTblCharacters = new List<TblCharacter>();
+		for (int x = 0; x < m_characters.TblCharacters.Length; x++)
+		{
+			TblCharacter currentCharacter = m_characters.TblCharacters[x];
+			if (GAME_ID == currentCharacter.IdtblGames)
+			{
+				listTblCharacters.Add(currentCharacter);
+			}
+		}
+		// Allocate array count based on Game-specific Characters found
+		m_rgPrefabClickableTexts = new GameObject[listTblCharacters.Count];
+		TblCharacter[] rgCharacters = listTblCharacters.ToArray();
+		for (int i = 0; i < m_rgPrefabClickableTexts.Length; i++)
+		{
+			m_rgPrefabClickableTexts[i] = Instantiate(Resources.Load("Prefabs/ClickableText")) as GameObject;
+			m_rgPrefabClickableTexts[i].transform.SetParent(m_panelCharactersList.transform);
+			ClickableText clickableText = m_rgPrefabClickableTexts[i].GetComponentInChildren<ClickableText>();
+			clickableText.m_entryPointPosition = i;
+			Text textClickableText = m_rgPrefabClickableTexts[i].GetComponentInChildren<Text>();
+			textClickableText.text = rgCharacters[i].ColName;
+		}
+	}
 }
