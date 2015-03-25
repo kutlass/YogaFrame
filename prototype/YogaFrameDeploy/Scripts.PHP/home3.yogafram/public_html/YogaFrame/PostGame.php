cc742ee5ea59c223582a76334d5c895face03f18
Karl Flores <kutlass@yogaframe.net>
2015-03-25 05:47:10 -0700

    Switch initialize housekeeping over to the new Initialize() method

--- prototype/YogaFrameDeploy/Scripts.PHP/home3.yogafram/public_html/YogaFrame/PostGame.php
+++ prototype/YogaFrameDeploy/Scripts.PHP/home3.yogafram/public_html/YogaFrame/PostGame.php
@@ -35,28 +35,9 @@ class PostGameHelper
             $fResult = Util::ExecuteQuery($mysqli, $strQuery);
             if (true != $fResult)
             {
-                //$dispatchFailure = new Dispatch();
-                //$dispatchFailure->Message = "PostGame.php: Failed to call the stored procedure.";
-                //Trace::WriteDispatchFailure($dispatchFailure);
-                
-                $jSessionResponse = new JSession();
-                $jSessionResponse->Dispatch = new Dispatch();
-                $jSessionResponse->Dapplers = new Dapplers();
-                $jSessionResponse->Dapplers->TblDapplers = array( new TblDappler() );
-                $jSessionResponse->InputSequences = new InputSequences();
-                $jSessionResponse->InputSequences->TblInputSequences = array( new TblInputSequence() );
-                $jSessionResponse->Sessions = new Sessions();
-                $jSessionResponse->Sessions->TblSessions = array( new TblSession() );
-                $jSessionResponse->Members = new Members();
-                $jSessionResponse->Members->TblMembers = array( new TblMember() );
-                $jSessionResponse->Moves = new Moves();
-                $jSessionResponse->Moves->TblMoves = array( new TblMove() );
-                $jSessionResponse->Games = new Games();
-                $jSessionResponse->Games->TblGames = array( new TblGame() );
-                $jSessionResponse->Characters = new Characters();
-                $jSessionResponse->Characters->TblCharacters = array( new TblCharacter() );
-                $jSessionResponse->Dispatch->Message = "PostGame.php: Failed to call the stored procedure.";
-                Trace::RespondToClientWithFailure($jSessionResponse);
+                $jSession = JSession::Initialize();
+                $jSession->Dispatch->Message = "PostGame.php: Failed to call the stored procedure.";
+                Trace::RespondToClientWithFailure($jSession);
             }
             
             $mysqli->close();
