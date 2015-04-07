6381ebbd4f5d9cf904a2d94d3d4b04d36c6d0c39
Karl Flores <kutlass@yogaframe.net>
2015-04-07 10:24:32 -0700

    Stub script for Profile scene.

---
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptProfile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using YogaFrameWebAdapter;
+using YogaFrameWebAdapter.Session;
+
+public class ScriptProfile : MonoBehaviour
+{
+	public Text m_textProfileAlias;
+	public Text m_textProfileNameFirst;
+	public Text m_textProfileYearsOfService;
+	public Text m_textBio;
+
+	// Use this for initialization
+	void Start()
+	{
+		m_textProfileAlias = null;
+		m_textProfileNameFirst = null;
+		m_textProfileYearsOfService = null;
+		m_textBio = null;
+
+		bool fResult = false;
+		fResult = ScriptProfile._GetMemberFields ();
+	}
+
+	private static bool _GetMemberFields()
+	{
+		bool fResult = false;
+		return fResult;
+	}
+}
