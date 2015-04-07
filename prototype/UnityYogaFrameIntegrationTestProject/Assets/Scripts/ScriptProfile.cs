6c68d7163cdaf81cd5d2704206097fd4fe6e765d
Karl Flores <kutlass@yogaframe.net>
2015-04-07 14:00:21 -0700

    Bind real web data for Profile scene.

--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptProfile.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptProfile.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections;
 using YogaFrameWebAdapter;
 using YogaFrameWebAdapter.Session;
+using YogaFrameWebAdapter.MembersJsonTypes;
 
 public class ScriptProfile : MonoBehaviour
 {
@@ -14,18 +16,41 @@ public class ScriptProfile : MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
-		m_textProfileAlias = null;
-		m_textProfileNameFirst = null;
-		m_textProfileYearsOfService = null;
-		m_textBio = null;
-
 		bool fResult = false;
-		fResult = ScriptProfile._GetMemberFields ();
+		fResult = ScriptProfile._GetMemberFields(
+			ref m_textProfileAlias,
+			ref m_textProfileNameFirst,
+			ref m_textProfileYearsOfService,
+			ref m_textBio
+			);
 	}
 
-	private static bool _GetMemberFields()
+	private static bool _GetMemberFields(
+		ref Text textProfileAlias,
+		ref Text textProfileNameFirst,
+		ref Text textProfileYearsOfService,
+		ref Text textBio
+		)
 	{
 		bool fResult = false;
+
+		if (
+			null == textProfileAlias ||
+			null == textProfileNameFirst ||
+			null == textProfileYearsOfService ||
+			null == textBio
+		    )
+		{
+			fResult = false;
+			throw new ArgumentNullException();
+		}
+
+		TblMember tblMember = Session.Instance.Cache.Members.TblMembers[0];
+		textProfileAlias.text = tblMember.ColNameAlias;
+		textProfileNameFirst.text = tblMember.ColNameFirst;
+		textProfileYearsOfService.text = "1"; // TODO: Replace hard-code with true logic
+		textBio.text = tblMember.ColBio;
+
 		return fResult;
 	}
 }
