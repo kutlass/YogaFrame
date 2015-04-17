b1d0c00497f94de3a98aa6692cea089d547d4e56
Karl Flores <kutlass@yogaframe.net>
2015-04-17 15:31:37 -0700

    Link stub scripts to SubmitMove and SubmitCharacter prefabs.

---
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/SubmitMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using YogaFrameWebAdapter;
+using YogaFrameWebAdapter.Session;
+using YogaFrameWebAdapter.CharactersJsonTypes;
+
+public class SubmitMove : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+}
