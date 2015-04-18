a596d08adf25b859862321d26586acf2c3c783b5
Karl Flores <kutlass@yogaframe.net>
2015-04-17 19:27:24 -0700

    Psuedo code for SubmitCharacter.cs

--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/SubmitCharacter.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/SubmitCharacter.cs
@@ -7,9 +7,44 @@ using YogaFrameWebAdapter.CharactersJsonTypes;
 
 public class SubmitCharacter : MonoBehaviour
 {
+	public int m_idtblGame;
+
+	//
+	// Field mappings for SubmitCharacter.prefab
+	//
+	public Text m_textCharacterGame;
+	public InputField m_inputFieldCharacterName;
+	public InputField m_inputFieldCharacterDescription;
+
 	// Use this for initialization
 	void Start()
 	{
-	
+		//
+		// Copy Game Name and ID
+		// 
+		int gamesPositionLastSelected = Session.Instance.Cache.GamesPositionLastSelected;
+		m_textCharacterGame.text = Session.Instance.Cache.Games.TblGames[gamesPositionLastSelected].ColName;
+		m_idtblGame = int.Parse(Session.Instance.Cache.Games.TblGames[gamesPositionLastSelected].IdtblGames);
+	}
+
+	public void SaveUI()
+	{
+		bool fResult = false;
+		fResult = SubmitCharacter._MemberPostCharacter(
+			m_inputFieldCharacterName.text,
+			m_inputFieldCharacterDescription.text
+			);
+	}
+
+	private static bool _MemberPostCharacter(
+		string strCharacterName,
+		string strCharacterDescription
+		)
+	{
+		bool fResult = false;
+		Characters characters = new Characters();
+		fResult = Session.Instance.MemberPostCharacter(ref characters);
+
+		return fResult;
 	}
 }
