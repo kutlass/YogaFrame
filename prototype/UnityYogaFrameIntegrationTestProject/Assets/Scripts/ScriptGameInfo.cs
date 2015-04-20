a1e51d2e6a2c7f1ab35e60e5ae4f6d2425fb19cc
Karl Flores <kutlass@yogaframe.net>
2015-04-20 06:41:33 -0700

    Fix cursor position bug for Character selection.

--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptGameInfo.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptGameInfo.cs
@@ -20,11 +20,17 @@ public class ScriptGameInfo : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		//
+		// Copy contextual Game info from cache
+		//
 		int position = Session.Instance.Cache.GamesPositionLastSelected;
 		m_textGameName.text = Session.Instance.Cache.Games.TblGames[position].ColName;
 		m_textGamePublisher.text = Session.Instance.Cache.Games.TblGames[position].ColPublisher;
 		m_textGameDescription.text = Session.Instance.Cache.Games.TblGames[position].ColDescription;
 
+		//
+		// Populate the Characters list for the Game at hand
+		//
 		_PopulateCharactersList();
 	}
 	
@@ -45,12 +51,14 @@ public class ScriptGameInfo : MonoBehaviour
 		int GAME_POSITION_LAST_SELECTED = Session.Instance.Cache.GamesPositionLastSelected;
 		string GAME_ID = Session.Instance.Cache.Games.TblGames[GAME_POSITION_LAST_SELECTED].IdtblGames;
 		List<TblCharacter> listTblCharacters = new List<TblCharacter>();
+		List<int> listIntPositions = new List<int>();
 		for (int x = 0; x < m_characters.TblCharacters.Length; x++)
 		{
 			TblCharacter currentCharacter = m_characters.TblCharacters[x];
 			if (GAME_ID == currentCharacter.IdtblGames)
 			{
 				listTblCharacters.Add(currentCharacter);
+				listIntPositions.Add(x);
 			}
 		}
 		// Allocate array count based on Game-specific Characters found
@@ -62,7 +70,7 @@ public class ScriptGameInfo : MonoBehaviour
 			m_rgPrefabClickableTexts[i].transform.SetParent(m_panelCharactersList.transform);
 			ClickableText clickableText = m_rgPrefabClickableTexts[i].GetComponentInChildren<ClickableText>();
 			clickableText.SetAssignedSceneToInvoke("SceneCharacterInfo");
-			clickableText.SetEntryPointPositionCharacters(i);
+			clickableText.SetEntryPointPositionCharacters(listIntPositions[i]);
 			Text textClickableText = m_rgPrefabClickableTexts[i].GetComponentInChildren<Text>();
 			textClickableText.text = rgCharacters[i].ColName;
 		}
