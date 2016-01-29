99c3bdb4e4dbc14e57b0c48524a9466e98cfe8a1
Karl Flores <kutlassx@hotmail.com>
2016-01-29 13:31:21 -0800

    Make unit test succeed: SessionMemberSignUp_MemberExistsEmailAddress()

--- prototype/YogaFrameDeploy/Scripts.PHP/home3.yogafram/public_html/YogaFrame/GandalfBridge.php
+++ prototype/YogaFrameDeploy/Scripts.PHP/home3.yogafram/public_html/YogaFrame/GandalfBridge.php
@@ -2,9 +2,9 @@
 
 class GandalfBridge
 {
-    const REG_EX_VALID_MEMBER_USER_NAME = "/^[a-zA-Z0-9]{2,14}$/";
-    const REG_EX_VALID_MEMBER_EMAIL_ADDRESS = "";
-    const REG_EX_VALID_MEMBER_PASSWORD = "/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/";
+    const REG_EX_VALID_MEMBER_USER_NAME     = "/^[a-zA-Z0-9]{2,14}$/";
+    const REG_EX_VALID_MEMBER_EMAIL_ADDRESS = "/^[a-zA-Z_]{2,18}@[a-zA-Z_]{2,18}.[a-zA-Z]{2,10}/$";
+    const REG_EX_VALID_MEMBER_PASSWORD      = "/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/";
 
     public static function ShallPassMemberUserName($strUserName)
     {
