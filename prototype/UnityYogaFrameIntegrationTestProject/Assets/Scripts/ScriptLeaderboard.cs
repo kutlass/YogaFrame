7fcb70639478c29747b37a78b5609506db08fd73
Karl Flores <kutlassx@hotmail.com>
2016-02-09 08:59:24 -0800

    Add Unity stub scripts for Leaderboard scene

---
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptLeaderboard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptLeaderboard : MonoBehaviour
+{
+    public GameObject m_panelLeaderboardsList;
+
+    // Use this for initialization
+    void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+}
