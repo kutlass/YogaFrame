7c8f28172d852eedff806b62e9aeab7e1189a827
Karl Flores <kutlassx@hotmail.com>
2016-02-11 14:13:09 -0800

    Convert tabs to spaces in ScriptSubmitGame.cs

--- prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptSubmitGame.cs
+++ prototype/UnityYogaFrameIntegrationTestProject/Assets/Scripts/ScriptSubmitGame.cs
@@ -8,86 +8,86 @@ using YogaFrameWebAdapter.GamesJsonTypes;
 
 public class ScriptSubmitGame : MonoBehaviour
 {
-	public InputField m_inputFieldGameName;
-	public InputField m_inputFieldPublisher;
-	public InputField m_inputFieldPublisherURL;
-	public InputField m_inputFieldDeveloper;
-	public InputField m_inputFieldDeveloperURL;
-	public InputField m_inputFieldDescription;
+    public InputField m_inputFieldGameName;
+    public InputField m_inputFieldPublisher;
+    public InputField m_inputFieldPublisherURL;
+    public InputField m_inputFieldDeveloper;
+    public InputField m_inputFieldDeveloperURL;
+    public InputField m_inputFieldDescription;
 
-	public GameObject m_gameObjectNavigateCurrent = null;
-	public GameObject m_gameObjectNavigateBack = null;
-	public GameObject m_gameObjectNavigateNext = null;
+    public GameObject m_gameObjectNavigateCurrent = null;
+    public GameObject m_gameObjectNavigateBack = null;
+    public GameObject m_gameObjectNavigateNext = null;
 
-	// Use this for initialization
-	void Start()
-	{
+    // Use this for initialization
+    void Start()
+    {
 	
-	}
+    }
 	
-	// Update is called once per frame
-	void Update()
-	{
+    // Update is called once per frame
+    void Update()
+    {
 	
-	}
+    }
 
-	public void NavigateBack()
-	{
-		m_gameObjectNavigateCurrent.SetActive(false);
-		m_gameObjectNavigateBack.SetActive(true);
-	}
+    public void NavigateBack()
+    {
+	    m_gameObjectNavigateCurrent.SetActive(false);
+	    m_gameObjectNavigateBack.SetActive(true);
+    }
 
-	public void NavigateNext()
-	{
-		m_gameObjectNavigateCurrent.SetActive(false);
-		m_gameObjectNavigateNext.SetActive(true);
-	}
+    public void NavigateNext()
+    {
+	    m_gameObjectNavigateCurrent.SetActive(false);
+	    m_gameObjectNavigateNext.SetActive(true);
+    }
 
-	public void SubmitGameUI()
-	{
-		bool fResult = false;
-		fResult = ScriptSubmitGame._SubmitGameWeb(
-			m_inputFieldGameName.text,
-			m_inputFieldPublisher.text,
-			m_inputFieldPublisherURL.text,
-			m_inputFieldDeveloper.text,
-			m_inputFieldDeveloperURL.text,
-			m_inputFieldDescription.text
-			);
-	}
+    public void SubmitGameUI()
+    {
+	    bool fResult = false;
+	    fResult = ScriptSubmitGame._SubmitGameWeb(
+            m_inputFieldGameName.text,
+            m_inputFieldPublisher.text,
+            m_inputFieldPublisherURL.text,
+            m_inputFieldDeveloper.text,
+            m_inputFieldDeveloperURL.text,
+            m_inputFieldDescription.text
+            );
+    }
 
-	private static bool _SubmitGameWeb(
-		string strGameName,
-		string strPublisher,
-		string strPublisherURL,
-		string strDeveloper,
-		string strDeveloperURL,
-		string strDescription
-		)
-	{
-		bool fResult = false;
-		JSession jSessionWebResponse = null;
-		List<TblGame> listTblGames = new List<TblGame>
-		{
-			new TblGame()
-			{
-				ColName         = strGameName,
-				ColDeveloper    = strDeveloper,
-				ColDeveloperURL = strDeveloperURL,
-				ColPublisher    = strPublisher,
-				ColPublisherURL = strPublisherURL,
-				ColDescription  = strDescription
-			}
-		};
-		Games games = new Games();
-		games.TblGames = listTblGames.ToArray();
-		jSessionWebResponse = WebAdapter.WebPostGame(ref games);
-		const string S_OK = "S_OK";
-		if (S_OK == jSessionWebResponse.Dispatch.Message)
-		{
-			fResult = true;
-		}
+    private static bool _SubmitGameWeb(
+        string strGameName,
+        string strPublisher,
+        string strPublisherURL,
+        string strDeveloper,
+        string strDeveloperURL,
+        string strDescription
+        )
+    {
+        bool fResult = false;
+        JSession jSessionWebResponse = null;
+        List<TblGame> listTblGames = new List<TblGame>
+        {
+            new TblGame()
+            {
+                ColName         = strGameName,
+                ColDeveloper    = strDeveloper,
+                ColDeveloperURL = strDeveloperURL,
+                ColPublisher    = strPublisher,
+                ColPublisherURL = strPublisherURL,
+                ColDescription  = strDescription
+            }
+        };
+        Games games = new Games();
+        games.TblGames = listTblGames.ToArray();
+        jSessionWebResponse = WebAdapter.WebPostGame(ref games);
+        const string S_OK = "S_OK";
+        if (S_OK == jSessionWebResponse.Dispatch.Message)
+        {
+	        fResult = true;
+        }
 
-		return fResult;
-	}
+        return fResult;
+    }
 }
